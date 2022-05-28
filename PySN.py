bf5a5d37def18e076535b31e3a4a4f74a3f4ba6a
Aphelion <61633628+AphelionWasTaken@users.noreply.github.com>
2022-05-28 10:00:29 -0500

    Add files via upload
    I'm bad at programming.

---
+++ PySN.py
@@ -0,0 +1,33 @@
+import hmac
+import hashlib
+import requests
+import xml.etree.ElementTree as ET
+from os.path import basename
+
+loop = 'Y'
+
+while(loop=='Y'):
+    title_id = input('Enter Title ID: ')
+
+    key = bytearray.fromhex('E5E278AA1EE34082A088279C83F9BBC806821C52F2AB5D2B4ABD995450355114')
+
+    id = bytes('np_' + title_id,'UTF-8')
+
+    hash = (hmac.new(key, id, hashlib.sha256).hexdigest())
+
+    xml_url = ('https://gs-sec.ww.np.dl.playstation.net/pl/np/' + title_id + '/' + hash + '/' + title_id + '-ver.xml')
+    print(xml_url)
+
+    var_url = requests.get(xml_url, verify=False)
+    if var_url.status_code == 200:
+        if var_url.text != '':
+            root = ET.fromstring(var_url.content)
+            for item in root.iter('package'):
+                url = (item.get('url'))
+                update_file = basename(url)
+                print(url)
+                open(update_file,'wb').write(requests.get(url).content)
+        else: print('No updates available for this game.')
+    else: print('Invalid Title ID')
+
+    loop = input('Check for more updates? Y/N: ')
