95f702a2e0eeda6ce425eeb65dbeb0b8a8abc37e
Aphelion <61633628+AphelionWasTaken@users.noreply.github.com>
2022-05-30 07:09:15 -0500

    Add support for PS4 Title Updates
    And try to make things look a little neater.

--- PySN.py
+++ PySN.py
@@ -7,19 +7,15 @@ from os import makedirs, path
 requests.packages.urllib3.disable_warnings() 
 
 loop = 'Y'
-key = bytearray.fromhex('E5E278AA1EE34082A088279C83F9BBC806821C52F2AB5D2B4ABD995450355114')
 
 while(loop=='Y'):
     title_id = input('Enter Title ID: ').upper()
-
+    key = bytearray.fromhex('E5E278AA1EE34082A088279C83F9BBC806821C52F2AB5D2B4ABD995450355114')
     id = bytes('np_' + title_id,'UTF-8')
-
     hash = (hmac.new(key, id, hashlib.sha256).hexdigest())
-    
     xml_url = ('https://gs-sec.ww.np.dl.playstation.net/pl/np/' + title_id + '/' + hash + '/' + title_id + '-ver.xml')
-
-    #I'm sure there's a better way to handle Sony's weird certs.
     var_url = requests.get(xml_url, verify=False)
+    #I'm sure there's a better way to handle Sony's weird certs.
 
     if var_url.status_code == 200:
         if var_url.text != '':
@@ -46,6 +42,7 @@ while(loop=='Y'):
     else:
         xml_url = 'https://a0.ww.np.dl.playstation.net/tpl/np/' + title_id + '/' + title_id + '-ver.xml'
         var_url = requests.get(xml_url, verify=False)
+
         if var_url.status_code == 200:
             if var_url.text != '':
                 root = ET.fromstring(var_url.content)
@@ -67,6 +64,36 @@ while(loop=='Y'):
                     open(download_path + '/' + update_file,'wb').write(requests.get(url).content)
                 print('Finished.')
             else: print('No updates available for this game.')
-        else: print('Invalid Title ID.')
+
+        else:
+            key = bytearray.fromhex('AD62E37F905E06BC19593142281C112CEC0E7EC3E97EFDCAEFCDBAAFA6378D84')
+            id = bytes('np_' + title_id,'UTF-8')
+            hash = (hmac.new(key, id, hashlib.sha256).hexdigest())
+            xml_url = ('https://gs-sec.ww.np.dl.playstation.net/plo/np/' + title_id + '/' + hash + '/' + title_id + '-ver.xml')
+            var_url = requests.get(xml_url, verify=False)
+
+            if var_url.status_code == 200:
+                if var_url.text != '':
+                    root = ET.fromstring(var_url.content)
+                    name = (root.find('./tag/package/paramsfo/title').text).replace('\n',' ')
+                    for item in root.iter('titlepatch'):
+                        titleid = item.get('titleid')
+           
+                    print('Found update(s) for: ' + titleid + ' ' + name)
+
+                    name = name.replace(':',' -').replace('/',' ').replace('?','')
+                    download_path = ('Updates/PlayStation 4/' + titleid + ' ' + name)
+                    if not path.exists(download_path):
+                       makedirs(download_path)
+
+                    for item in root.iter('delta_info_set'):
+                        url = (item.get('url'))
+                        update_file = path.basename(url)
+                        print('downloading ' + update_file)
+                        open(download_path + '/' + update_file,'wb').write(requests.get(url).content)
+                    print('Finished.')
+                else: print('No updates available for this game.')
+
+            else: print('Invalid Title ID.')
 
     loop = input('Check for more updates? Y/N: ').upper()
