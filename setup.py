1d9689447536b6e196ed652d3939adfa908d9a10
Aphelion <61633628+AphelionWasTaken@users.noreply.github.com>
2023-02-01 20:43:37 -0600

    Minor refactor (#10)

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-# "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["requests","clint"],  "excludes": ["tkinter", "test", "distutils"]}
+# 'packages': ['os'] is used as example only
+build_exe_options = {'packages': ['requests','clint'],  'excludes': ['tkinter', 'test', 'distutils']}
 
 setup(
-    name="PySN",
-    version="0.1",
-    description="PySN Title Update Utility",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("PySN.py")],
+    name='PySN',
+    version='1.1.0',
+    description='PySN Title Update Utility',
+    options={'build_exe': build_exe_options},
+    executables=[Executable('PySN.py')],
 )
