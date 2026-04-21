8c08aec83a9bcd1e8ea9922b9591c96657a6d9b3
Aphelion <61633628+AphelionWasTaken@users.noreply.github.com>
2026-04-21 01:48:46 -0500

    Add icon to windows

--- PySN.py
+++ PySN.py
@@ -111,6 +111,7 @@ class SettingsWindow(customtkinter.CTkToplevel):
         self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
         self.grid_columnconfigure((0, 1, 2, 3, 4, 5), weight=1)
         self.temp_save, self.temp_rpcs3 = ConfigSettings.check_config()
+        self.after(200, lambda: self.iconbitmap('AphIcon.ico'))
 
         self.save_dir_label = customtkinter.CTkLabel(master=self, text='Download Update PKGs To This Folder:',justify = 'center', anchor='center')
         self.save_dir_label.grid(row=0, column=1, columnspan=4, padx=5, pady=(20,0), sticky='sew')
@@ -184,6 +185,7 @@ class DownloadAllWindow(customtkinter.CTkToplevel):
         self.geometry('400x240')
         self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
         self.grid_columnconfigure((0, 1, 2, 3, 4, 5), weight=1)
+        self.after(200, lambda: self.iconbitmap('AphIcon.ico'))
 
         self.save_dir_label = customtkinter.CTkLabel(master=self, text='You are about to download all of the updates in the list!\n\n\nContinue?\n', anchor='center')
         self.save_dir_label.grid(row=1, column=2, columnspan=2, padx=20, pady=0, sticky='ew')
@@ -292,6 +294,7 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure((0, 1, 2), weight=1)
         self.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
         self.bind('<Return>', lambda event: self.button_search())
+        self.iconbitmap('AphIcon.ico')
 
         self.entry = customtkinter.CTkEntry(master=self, placeholder_text='Enter Serial', width = 125)
         self.entry.grid(row=0, column=0, padx=(4,2), pady=(6,0), sticky='ew')
@@ -342,13 +345,14 @@ class App(customtkinter.CTk):
                 home_dir = os.environ['HOME']
                 yml_dir = home_dir+'/.config/rpcs3/games.yml'
             else: yml_dir = rpcs3_dir+'config/games.yml'
-            with open(yml_dir, 'r') as f:
-                file = yaml.safe_load(f)
-                alist = list(file)
-                for index in alist:
-                    title_id = index
-                    self.search(title_id, console)
-                    self.search_no_drm(title_id, console)
+            if os.path.isfile(yml_dir):
+                with open(yml_dir, 'r') as f:
+                    file = yaml.safe_load(f)
+                    alist = list(file)
+                    for index in alist:
+                        title_id = index
+                        self.search(title_id, console)
+                        self.search_no_drm(title_id, console)
         self.button1.configure(state = 'normal')
         self.button2.configure(state = 'normal')
         self.button3.configure(state = 'normal')
